a73368acb3e5f43ce9e8581f47aee146cb6c72ae
unknown <germanllop@gmail.com>
2020-02-27 21:43:03 -0500

    add morgan and env port

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const session = require('express-session')
+const morgan = require('morgan')
 
 const app = express()
 const api = require('./routes/api')
 const auth = require('./routes/auth')
 // const models = require('./models/models')
 
-const port = 3000
-
 require('dotenv').config()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+app.use(morgan('dev'))
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -27,6 +27,8 @@ app.get('/', (req, res) => res.send('Hello Worldss'))
 
 app.use('/api', api)
 app.use('/auth', auth)
+
+const port = process.env.PORT || 3000
 
 mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }).then(async() => {
   app.listen(port, () => console.log('App listening on port ' + port + ': Go to http://localhost:' + port))
